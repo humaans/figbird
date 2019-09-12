1ef95dccc60b6e11d8dc9fb7a5c56523048b50df
Karolis Narkevicius <karolis.n@gmail.com>
2019-09-12 16:15:57 +0100

    Working build process

--- tasks/build.js
+++ tasks/build.js
@@ -1,13 +1,14 @@
 const execa = require('execa')
 
+const babel = './node_modules/.bin/babel'
 const sh = (...args) => execa(...args, { stdio: 'inherit', shell: true })
+
+const watch = process.argv[2] === '-w'
+const w = watch ? ' -w' : ''
 
 ;(async function() {
   await sh('rm -rf dist')
-  await sh(`cp -R lib dist`)
-
-  const pkg = require('../package.json')
-  const babel = './node_modules/.bin/babel'
-  await sh(`${babel} --no-babelrc lib/index.js -o dist/${pkg.main} --config-file=./.babelrc-cjs`)
-  await sh(`${babel} --no-babelrc lib/index.js -o dist/${pkg.module} --config-file=./.babelrc-esm`)
+  await sh('mkdir -p dist')
+  await sh(`${babel}${w} --verbose --no-babelrc lib -d dist/esm --config-file=./.babelrc-esm`)
+  await sh(`${babel}${w} --verbose --no-babelrc lib -d dist/cjs --config-file=./.babelrc-cjs`)
 })()
