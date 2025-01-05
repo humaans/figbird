c8ae5f75f5ddb7e56a762cb9915eaad12f1b84bd
Karolis Narkevicius <karolis.n@gmail.com>
2025-01-05 15:51:33 +0000

    More feats

--- test/figbird.test.js
+++ test/figbird.test.js
@@ -777,7 +777,7 @@ test('useFind with allPages', async t => {
   unmount()
 })
 
-test.only('useFind with allPages without total', async t => {
+test('useFind with allPages without total', async t => {
   const { render, flush, unmount, $all } = dom()
   function Note() {
     const notes = useFind('notes', { query: { $limit: 1 }, allPages: true })
