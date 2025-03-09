8279d70fcbaddcc133490532e38c2b998e59bf4f
Karolis Narkevicius <karolis.n@gmail.com>
2025-03-09 18:47:21 +0000

    Ignore dist when linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import pluginReact from 'eslint-plugin-react'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['docs', 'coverage'],
+    ignores: ['docs', 'coverage', 'dist'],
   },
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
